91eee6d77cd283dffb5b819326a6eb845a544b92
Artem Svetlov <trolleway@yandex.ru>
2022-08-06 13:04:44 +0300

    liaz677

--- tmp_custom.sql
+++ tmp_custom.sql
@@ -1,89 +1,71 @@
-UPDATE photos SET city="Moscow", sublocation="",datetime="2005-05-11T16:15:20", wkt_geometry="POINT (37.62118 55.68075)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-563_20050511_365.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2006-08-15T15:58:02", wkt_geometry="POINT (37.64246 55.71628)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-616_20060815_057.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2004-10-28T16:30:37", wkt_geometry="POINT (37.62081 55.70259)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-618_20041028.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2005-07-07T18:29:08", wkt_geometry="POINT (37.68509 55.63231)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-693_20050707_51.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2006-07-03T15:38:11", wkt_geometry="POINT (37.68510 55.63222)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-709_20060703_243.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2007-07-14T15:09:29", wkt_geometry="POINT (37.57795 55.55001)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-737_20070714_044.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2005-07-19T17:59:41", wkt_geometry="POINT (37.56709 55.74309)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-738_20050719_046.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2005-04-28T16:34:08", wkt_geometry="POINT (37.62087 55.68362)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-743_20050428_071.jpg"  ;
- UPDATE photos SET city="Ramenskoe", sublocation="",datetime="2004-07-12T15:21:59", wkt_geometry="POINT (38.22543 55.56557)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-749_20040712_0001.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2005-07-20T18:07:55", wkt_geometry="POINT (37.62246 55.66312)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-756_20050720_034.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2005-08-24T18:24:48", wkt_geometry="POINT (37.67556 55.62385)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-760_20050824_048.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2005-07-07T16:23:11", wkt_geometry="POINT (37.62363 55.70464)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-768_20050707_11.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2005-08-21T16:54:22", wkt_geometry="POINT (37.66372 55.57279)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-770_20050821_126.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2006-01-16T13:28:24", wkt_geometry="POINT (37.61614 55.69601)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-771_20060116_056.jpg"  ;
- UPDATE photos SET city="Vidnoe", sublocation="",datetime="2006-10-02T15:43:28", wkt_geometry="POINT (37.68509 55.54894)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-771_20061002_115.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2007-05-05T17:08:50", wkt_geometry="POINT (37.71497 55.65982)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-778_20070505_081.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2004-08-24T09:59:53", wkt_geometry="POINT (37.65550 55.77386)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-794_20040824_1.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2007-06-10T15:56:35", wkt_geometry="POINT (37.68088 55.62760)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-808_20070610_109.jpg"  ;
- UPDATE photos SET city="Vidnoe", sublocation="",datetime="2005-08-21T16:22:27", wkt_geometry="POINT (37.66692 55.56732)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-812_20050821_112.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2005-01-03T16:05:41", wkt_geometry="POINT (37.65469 55.59176)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-817_20050103_197.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2005-10-20T17:31:08", wkt_geometry="POINT (37.66291 55.75617)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-819_20051020_027.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2005-08-03T10:49:02", wkt_geometry="POINT (37.68904 55.64000)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-820_20050803_002.jpg"  ;
- UPDATE photos SET city="Lubertsy", sublocation="",datetime="2005-08-10T16:51:47", wkt_geometry="POINT (37.92391 55.66934)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-828_20050810_121.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2005-04-11T16:04:26", wkt_geometry="POINT (37.62103 55.68317)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-830_20050411_2.jpg"  ;
- UPDATE photos SET city="Domodedovo", sublocation="",datetime="2006-08-19T14:54:48", wkt_geometry="POINT (37.77300 55.43980)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-835_20060819_084.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2005-07-03T18:11:10", wkt_geometry="POINT (37.62683 55.63232)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-841_20050703_126.jpg"  ;
- UPDATE photos SET city="Ramenskoe", sublocation="",datetime="2004-06-27T10:06:49", wkt_geometry="POINT (38.24110 55.55986)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-843_20040627_013.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2004-07-25T19:34:49", wkt_geometry="POINT (37.70514 55.78121)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-844_20040725_002.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2006-09-02T18:58:59", wkt_geometry="POINT (37.70226 55.73393)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-853_20060902_188.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2006-03-06T13:44:25", wkt_geometry="POINT (37.66117 55.57671)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-858_20060306_035.jpg"  ;
- UPDATE photos SET city="Ramenskoe", sublocation="",datetime="2005-05-29T13:04:48", wkt_geometry="POINT (38.29661 55.54010)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-864_20050529_385.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2005-08-06T10:56:27", wkt_geometry="POINT (37.65978 55.76225)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-865_20050806_055.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2007-03-18T16:10:21", wkt_geometry="POINT (37.64452 55.61181)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-868_20070318_147.jpg"  ;
- UPDATE photos SET city="Zukovsky", sublocation="",datetime="2004-05-30T13:17:46", wkt_geometry="POINT (38.13595 55.60181)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-871_20040530_029.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2004-09-24T18:25:46", wkt_geometry="POINT (37.68995 55.64162)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-889_20040924_007.jpg"  ;
- UPDATE photos SET city="Kolomna", sublocation="",datetime="2005-08-28T18:28:49", wkt_geometry="POINT (38.78100 55.09948)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-892_20050828_169.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2005-07-08T15:13:19", wkt_geometry="POINT (37.68887 55.64003)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-895_20050708_006.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2006-06-16T16:38:16", wkt_geometry="POINT (37.68936 55.64045)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-901_20060616_067.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2007-07-19T20:06:25", wkt_geometry="POINT (37.70310 55.73322)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-909_20070719_046.jpg"  ;
- UPDATE photos SET city="Lubertsy", sublocation="",datetime="2005-10-01T16:01:29", wkt_geometry="POINT (37.86859 55.69634)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-911_20051001_044.jpg"  ;
- UPDATE photos SET city="Zukovsky", sublocation="",datetime="2005-06-04T10:16:47", wkt_geometry="POINT (38.16180 55.59114)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-915_20050604_013.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2007-07-08T15:43:37", wkt_geometry="POINT (37.68666 55.63650)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-922_20070708_044.jpg"  ;
- UPDATE photos SET city="", sublocation="",datetime="2004-07-12T17:25:05", wkt_geometry="POINT (38.78054 55.38916)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-931_20040712_0059.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2006-08-15T15:56:17", wkt_geometry="POINT (37.64262 55.71625)"
-            WHERE hotlink="https://trolleway.com/storage/rail/RZD ChS2/ChS2-943_20060815_053.jpg"  ;
+UPDATE photos SET city="Moscow", sublocation="Sokolinaya Gora",datetime="2020-07-01T14:16:22", wkt_geometry="POINT (37.72523 55.76200)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200701_01_abandoned_station.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Sokolinaya Gora",datetime="2020-07-01T14:16:22", wkt_geometry="POINT (37.72346 55.76143)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200701_02 CHME3T.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Sokolinaya Gora",datetime="2020-07-01T14:16:22", wkt_geometry="POINT (37.72336 55.76156)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200701_03 ChME3.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Perovo",datetime="2020-07-03T07:17:16", wkt_geometry="POINT (37.79979 55.74672)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200703_04 Perovo library.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Perovo",datetime="2020-07-03T07:17:16", wkt_geometry="POINT (37.77461 55.74648)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200703_06 Lazo street Perovo.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Perovo",datetime="2020-07-03T07:17:16", wkt_geometry="POINT (37.78412 55.74590)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200703_07 Perovo shop_ShiftN.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Perovo",datetime="2020-07-03T07:17:16", wkt_geometry="POINT (37.78015 55.74612)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200703_08 Moscow trolleybus perovo .jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Perovo",datetime="2020-07-03T07:17:16", wkt_geometry="POINT (37.76068 55.75268)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200703_09 Lefortovo Perovo railway.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Perovo",datetime="2020-07-03T07:17:16", wkt_geometry="POINT (37.76067 55.75266)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200703_10 Lefortovo Perovo railway.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Perovo",datetime="2020-07-03T07:17:16", wkt_geometry="POINT (37.75466 55.75177)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200703_11 Lefortovo Perovo railway.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Perovo",datetime="2020-07-03T07:17:16", wkt_geometry="POINT (37.75463 55.75177)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200703_12 Lefortovo Perovo railway.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="",datetime="2020-07-04T15:19:36", wkt_geometry="POINT (37.69389 55.72846)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200704_13 Boinya station.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="",datetime="2020-07-04T15:19:36", wkt_geometry="POINT (37.69175 55.72566)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200704_14 Boinya station.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="",datetime="2020-07-04T15:19:36", wkt_geometry="POINT (37.69063 55.72541)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200704_15 Boinya station.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-04T15:19:36", wkt_geometry="POINT (37.67820 55.77317)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200704_16 Baumanskaya street.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-04T15:19:36", wkt_geometry="POINT (37.67801 55.77315)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200704_17 Bakuninskaya street.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-04T15:19:36", wkt_geometry="POINT (37.67789 55.77314)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200704_18 Bakuninskaya street.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-04T15:19:36", wkt_geometry="POINT (37.67761 55.77308)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200704_19 Bakuninskaya street.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.66356 55.77193)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_25 Novoryazanskaya street.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.66371 55.77206)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_26 Novoryazanskaya street.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.66322 55.77082)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_27.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.66189 55.76983)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_28.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.67465 55.77619)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_29.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.67477 55.77655)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_30.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.67474 55.77654)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_31.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.66832 55.77478)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_32.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.66398 55.77324)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_33.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.66407 55.77203)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_34 Novoryazanskaya street.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.66232 55.76979)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_35.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.66224 55.76982)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_36.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="",datetime="2021-07-05T17:19:59", wkt_geometry="POINT (37.78273 55.65079)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20210706_20 Marino.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="",datetime="2021-07-05T17:19:59", wkt_geometry="POINT (37.71990 55.64136)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20210706_21 Marino.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="",datetime="2021-07-05T17:19:59", wkt_geometry="POINT (37.69665 55.58675)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20210706_22 Zagorie railway.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="",datetime="2021-07-05T17:19:59", wkt_geometry="POINT (37.68734 55.62927)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20210706_23 Tsaritsino pond.jpg"  ;
+ UPDATE photos SET city="Moscow", sublocation="",datetime="2021-07-05T17:19:59", wkt_geometry="POINT (37.66074 55.76086)"
+            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20210706_24 Kursky station.jpg"  ;
  
