46b9478869cc500502f0f8bd71576a4e05ab534d
Artem Svetlov <trolleway@yandex.ru>
2022-08-06 15:08:06 +0300

    khrapunovo

--- tmp_custom.sql
+++ tmp_custom.sql
@@ -1,71 +1,43 @@
-UPDATE photos SET city="Moscow", sublocation="Sokolinaya Gora",datetime="2020-07-01T14:16:22", wkt_geometry="POINT (37.72523 55.76200)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200701_01_abandoned_station.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Sokolinaya Gora",datetime="2020-07-01T14:16:22", wkt_geometry="POINT (37.72346 55.76143)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200701_02 CHME3T.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Sokolinaya Gora",datetime="2020-07-01T14:16:22", wkt_geometry="POINT (37.72336 55.76156)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200701_03 ChME3.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Perovo",datetime="2020-07-03T07:17:16", wkt_geometry="POINT (37.79979 55.74672)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200703_04 Perovo library.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Perovo",datetime="2020-07-03T07:17:16", wkt_geometry="POINT (37.77461 55.74648)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200703_06 Lazo street Perovo.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Perovo",datetime="2020-07-03T07:17:16", wkt_geometry="POINT (37.78412 55.74590)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200703_07 Perovo shop_ShiftN.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Perovo",datetime="2020-07-03T07:17:16", wkt_geometry="POINT (37.78015 55.74612)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200703_08 Moscow trolleybus perovo .jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Perovo",datetime="2020-07-03T07:17:16", wkt_geometry="POINT (37.76068 55.75268)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200703_09 Lefortovo Perovo railway.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Perovo",datetime="2020-07-03T07:17:16", wkt_geometry="POINT (37.76067 55.75266)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200703_10 Lefortovo Perovo railway.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Perovo",datetime="2020-07-03T07:17:16", wkt_geometry="POINT (37.75466 55.75177)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200703_11 Lefortovo Perovo railway.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Perovo",datetime="2020-07-03T07:17:16", wkt_geometry="POINT (37.75463 55.75177)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200703_12 Lefortovo Perovo railway.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2020-07-04T15:19:36", wkt_geometry="POINT (37.69389 55.72846)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200704_13 Boinya station.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2020-07-04T15:19:36", wkt_geometry="POINT (37.69175 55.72566)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200704_14 Boinya station.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2020-07-04T15:19:36", wkt_geometry="POINT (37.69063 55.72541)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200704_15 Boinya station.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-04T15:19:36", wkt_geometry="POINT (37.67820 55.77317)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200704_16 Baumanskaya street.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-04T15:19:36", wkt_geometry="POINT (37.67801 55.77315)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200704_17 Bakuninskaya street.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-04T15:19:36", wkt_geometry="POINT (37.67789 55.77314)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200704_18 Bakuninskaya street.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-04T15:19:36", wkt_geometry="POINT (37.67761 55.77308)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200704_19 Bakuninskaya street.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.66356 55.77193)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_25 Novoryazanskaya street.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.66371 55.77206)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_26 Novoryazanskaya street.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.66322 55.77082)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_27.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.66189 55.76983)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_28.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.67465 55.77619)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_29.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.67477 55.77655)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_30.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.67474 55.77654)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_31.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.66832 55.77478)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_32.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.66398 55.77324)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_33.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.66407 55.77203)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_34 Novoryazanskaya street.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.66232 55.76979)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_35.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="Baumanskaya",datetime="2020-07-06T15:21:54", wkt_geometry="POINT (37.66224 55.76982)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20200706_36.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2021-07-05T17:19:59", wkt_geometry="POINT (37.78273 55.65079)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20210706_20 Marino.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2021-07-05T17:19:59", wkt_geometry="POINT (37.71990 55.64136)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20210706_21 Marino.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2021-07-05T17:19:59", wkt_geometry="POINT (37.69665 55.58675)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20210706_22 Zagorie railway.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2021-07-05T17:19:59", wkt_geometry="POINT (37.68734 55.62927)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20210706_23 Tsaritsino pond.jpg"  ;
- UPDATE photos SET city="Moscow", sublocation="",datetime="2021-07-05T17:19:59", wkt_geometry="POINT (37.66074 55.76086)"
-            WHERE hotlink="https://trolleway.com/storage/2020/2020-07-01_film/20210706_24 Kursky station.jpg"  ;
+UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T15:25:40", wkt_geometry="POINT (38.35465 55.72097)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/Khrapunovo industrial railway_20211107_07.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T15:30:23", wkt_geometry="POINT (38.35384 55.72121)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/Khrapunovo industrial railway_20211107_09.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T15:33:52", wkt_geometry="POINT (38.35247 55.72152)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/Khrapunovo industrial railway_20211107_10.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T15:36:33", wkt_geometry="POINT (38.35129 55.72175)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/Khrapunovo industrial railway_20211107_11.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T15:36:48", wkt_geometry="POINT (38.35129 55.72175)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/Khrapunovo industrial railway_20211107_12.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T15:36:58", wkt_geometry="POINT (38.35129 55.72175)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/Khrapunovo industrial railway_20211107_14.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T15:42:49", wkt_geometry="POINT (38.34913 55.72323)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/Khrapunovo industrial railway_20211107_15.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T15:51:33", wkt_geometry="POINT (38.35086 55.72175)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/Khrapunovo industrial railway_20211107_17.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T15:54:36", wkt_geometry="POINT (38.34816 55.72186)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/Khrapunovo industrial railway_20211107_18.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T15:55:26", wkt_geometry="POINT (38.34790 55.72187)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/Khrapunovo industrial railway_20211107_19.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T15:55:31", wkt_geometry="POINT (38.34790 55.72187)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/Khrapunovo industrial railway_20211107_20.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T15:57:16", wkt_geometry="POINT (38.34667 55.72189)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/Khrapunovo industrial railway_20211107_21.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T15:58:46", wkt_geometry="POINT (38.34561 55.72189)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/Khrapunovo industrial railway_20211107_23.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T16:01:48", wkt_geometry="POINT (38.34303 55.72202)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/Khrapunovo industrial railway_20211107_26.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T16:01:55", wkt_geometry="POINT (38.34303 55.72202)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/Khrapunovo industrial railway_20211107_27.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T15:14:08", wkt_geometry="POINT (38.34860 55.72097)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/khrapunovo station_20211107_01.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T15:14:15", wkt_geometry="POINT (38.34860 55.72097)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/khrapunovo station_20211107_02.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T16:09:25", wkt_geometry="POINT (38.33943 55.72312)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/Khrapunovo town magnit_20211107_31.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T16:06:23", wkt_geometry="POINT (38.34013 55.72248)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/Khrapunovo town_20211107_29.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T16:06:58", wkt_geometry="POINT (38.33990 55.72267)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/Khrapunovo town_20211107_30.jpg"  ;
+ UPDATE photos SET city="Khrapunovo", sublocation="",datetime="2021-11-07T15:22:03", wkt_geometry="POINT (38.35356 55.72086)"
+            WHERE hotlink="https://trolleway.com/storage/2021/2021-11-khrapunovo/VL10U-924_20211107_03_crop.jpg"  ;
  
