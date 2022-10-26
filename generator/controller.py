64996e2d0ba02010908bbcbfeef8195c1c646496
Artem Svetlov <trolleway@yandex.ru>
2022-10-27 02:52:19 +0300

    more schema.org

--- generator/controller.py
+++ generator/controller.py
@@ -466,8 +466,12 @@ class Website_generator():
                 if photo4template['city'] != '': photo4template['city']+='.'
                 if photo4template['sublocation'] != '': photo4template['sublocation']+='.'
 
-                schema_org_js = '''<script type="application/ld+json">{"@context":"https:\/\/schema.org","@type":"ImageObject","contentUrl":"{contentUrl}","license":"https:\/\/creativecommons.org\/licenses\/by\/4.0","acquireLicensePage":"{page_url}"}</script>'''
+                schema_org_js = '''<script type="application/ld+json">{"@context":"https:\/\/schema.org","@type":"ImageObject","contentUrl":"{contentUrl}","license":"https:\/\/creativecommons.org\/licenses\/by\/4.0","acquireLicensePage":"{page_url}, 
+                "creator": {"@type": "Person","name": "Artem Svetlov"},
+                "contentLocation": {"@type": "Place", "geo": {    "@type": "GeoCoordinates",    "latitude": "{lat}",    "longitude": "{lon}"  }}"}</script>'''
                 schema_org_js=schema_org_js.replace('{contentUrl}',photo4template['image_url'])
+                schema_org_js=schema_org_js.replace('{lat}',str(lat or ''))
+                schema_org_js=schema_org_js.replace('{lon}',str(lon or ''))
                 schema_org_js=schema_org_js.replace('{page_url}',photo4template['page_url_absolute'].replace('/','\/'))
                 photo4template['schema_org_js'] = schema_org_js
                 
