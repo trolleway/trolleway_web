b573ffc6b40721981ceb85eb1bfd29512968a7c1
Artem Svetlov <trolleway@yandex.ru>
2022-12-01 02:16:24 +0300

    mazur

--- interface.sh
+++ interface.sh
@@ -12,25 +12,68 @@ python3 generator/thumbnails-parallel.py
 }
 
 #------------------------
+webmap_update() {
+python3 generator/webmap_password.py
 
-import_dir() {
-dialog --title "Choose any file in folder" --fselect ~ 30 60
-FILE=$(dialog --stdout --title "Choose any file in folder with JPG and gpx" --fselect $HOME/ 14 48)
-echo "${FILE} file chosen."
+}
+#------------------------
+exif2sidecar() {
+
+EXIFTOOL=/opt/exiftool/exiftool
+
+HOME=/opt/images_origins
+FILE=$(dialog --stdout --title "Choose any file in folder with JPG " --fselect $HOME/ 14 48)
 
 
 if   [ -d "${FILE}" ]
 then echo 'dir';
 elif [ -f "${FILE}" ]
 then FILE=$(dirname $FILE);
-else echo "${FILE} is not valid";
+else 
+	echo "${FILE} is not valid";
+	whiptail --title "Test Message Box" --msgbox "${FILE} is not valid" 10 60
+	return 0
      
+fi
+
+if (whiptail --title  "Yes/No Box" --yesno "Generate sidecar wxif files for directory ${FILE}" 10 60)  then
+	 whiptail --title "Test Message Box" --msgbox "exiftool -ext JPG -o %d%f.xmp -r ${FILE}" 10 60
+	 
+	 $EXIFTOOL -ext JPG -o %d%f.xmp -r ${FILE}
+	 $EXIFTOOL -ext jpg -o %d%f.xmp -r ${FILE}
+	 $EXIFTOOL -ext TIF -o %d%f.xmp -r ${FILE}
+	 $EXIFTOOL -ext tif -o %d%f.xmp -r ${FILE}
+else
+     return 0
 fi
 
 
 
-echo ${FILE}
+
 read -n 1 -s -r -p "Press any key to continue"
+}
+
+#------------------------
+
+import_dir() {
+
+HOME=/opt/images_origins
+FILE=$(dialog --stdout --title "Choose any file in folder with JPG " --fselect $HOME/ 14 48)
+
+
+if   [ -d "${FILE}" ]
+then echo 'dir';
+elif [ -f "${FILE}" ]
+then FILE=$(dirname $FILE);
+else 
+	echo "${FILE} is not valid";
+	whiptail --title "Test Message Box" --msgbox "${FILE} is not valid" 10 60
+	return 0
+     
+fi
+
+
+python3 generator/dir2db.py ${FILE}
 
 }
 
@@ -48,6 +91,8 @@ $DIALOG --clear --title "Select operation" \
         "generate_html"  "Generate HTML" \
         "rebuild_thumbnails"  "Rebuild thumbnails" \
         "import_dir"  "import_dir" \
+        "webmap_update"  "webmap_update" \
+        "exif2sidecar"  "exif2sidecar" \
         "exit"  "Exit" 2> $tempfile
 
 retval=$?
@@ -63,8 +108,8 @@ case $retval in
         generate_html) generate_html;;
         rebuild_thumbnails) rebuild_thumbnails;;
         import_dir) import_dir;;
-        xiaomi360_logo) xiaomi360_logo;;
-        gopro_video) gopro_video;;
+        webmap_update) webmap_update;;
+        exif2sidecar) exif2sidecar;;
         mp4_merge) mp4_merge;;
         exit) exit;;
     esac;;
