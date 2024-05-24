9500c03db30d08803b5de5a2d46a8fc58d363a7b
Artem Svetlov <trolleway@yandex.ru>
2024-05-24 23:54:24 +0300

    upd

--- start.ps1
+++ start.ps1
@@ -1,3 +1,4 @@
+docker build -f Dockerfile -t trolleway_website:dev .
 docker run --rm -it -v ${PWD}:/opt/website -v c:\trolleway\website-storage\storage\:/opt/storage -v c:\trolleway\website-storage\master\:/opt/images_origins  trolleway_website:dev  /bin/bash
 
 pause
